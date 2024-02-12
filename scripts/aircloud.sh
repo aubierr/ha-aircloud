b58f36ea6199e71e443864837fa62646a6d3dffc
aubierr <74046434+aubierr@users.noreply.github.com>
2024-02-12 19:54:07 +0100

    Update aircloud.sh
    -Changing the order of parameters
    -Adding default setting
    -Added FAN mode
    -Added DRY mode

--- scripts/aircloud.sh
+++ scripts/aircloud.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+##########################################################
+# ./airCloud.sh <action> <room> <temperature> <mode> <fanspeed> <fanswing>
+#
+# <action> : on (Power On) / off (Power Off) / powerstatus (AC Power Status) / modestatus (AC Mode Status) / roomtemperature (Room Temperature) / idutemperature (AC Thermostat Temperature) / websocatdebug (display Hitachi websocket full result in json)
+# <room> : Name of the room on the Aircloud application
+# <temperature> : Target temperature
+# <mode> : COOLING / HEATING / FAN / DRY / AUTO(defaut)
+# <fanspeed> : LV1 / LV2 / LV3 / LV4 / LV5 / AUTO(defaut)
+# <fanswing> :  VERTICAL / OFF(defaut)
+##########################################################
+
+
 #login and password in base64
 hitachiuser=$(echo "<replace with your Hitachi's account email in base64>" | base64 -d)
 hitachipassword=$(echo "<replace with your Hitachi's account password in base64>" | base64 -d)
@@ -23,6 +36,17 @@ cloudlds=$(curl -s -H "Authorization: Bearer $token" -H "Content-Type: applicati
 #echo $cloudlds
 connectandsub=$(printf "CONNECT\naccept-version:1.1,1.2\nheart-beat:10000,10000\nAuthorization:Bearer $token\n\n\0\nSUBSCRIBE\nid:$uuid\ndestination:/notification/$familyId/$familyId\nack:auto\n\n\0" | base64 -w0 )
 
+# echo "#########################################"
+# echo "Order parameters"
+# echo "1 :" $1
+# echo "2 :" $2
+# echo "3 :" $3
+# echo "4 :" $4
+# echo "5 :" $5
+# echo "6 :" $6
+# echo "#########################################"
+
+
 if [ "$2" = "" ]
 	then
 		:
@@ -38,9 +62,44 @@ if [ "$2" = "" ]
 		  done
 fi
 
-mode=$3
-temperature=$4
+temperature=$3".0"
+mode=$4
 fanSpeed=$5
+fanSwing=$6
+
+if [ "$4" = "" ]
+	then
+		mode="AUTO"
+	elif [ "$4" = "FAN" ]; then
+		temperature="0"
+	else
+		:
+fi
+
+if [ "$5" = "" ] && [ "$4" != "FAN" ]
+	then
+		fanSpeed="AUTO"
+	elif [ "$5" = "" ] && [ "$4" = "FAN" ]; then
+        fanSpeed="LV2"
+	else
+		:
+fi
+
+if [ "$6" = "" ]
+	then
+		fanSwing="OFF"
+	else
+		:
+fi
+
+# echo "#########################################"
+# echo "Content of variables"
+# echo "Room :" $roomName "    ID :" $roomId
+# echo "Temperature :" $temperature
+# echo "Mode :" $mode
+# echo "Fan Speed :" $fanSpeed
+# echo "Fan Swing :" $fanSwing
+# echo "#########################################"
 
 case "$1" in
 "on")
@@ -55,7 +114,7 @@ case "$1" in
 	until [ $(echo $websocatresult | jq -r ".data[] | select(.id==$roomId) | .power") = "ON" ] && [ $(echo $websocatresult | jq -r ".data[] | select(.id==$roomId) | .iduTemperature") -eq $(echo $temperature) ] && [ $(echo $websocatresult | jq -r ".data[] | select(.id==$roomId) | .fanSpeed") = $(echo $fanSpeed) ]
 	do
 		#echo "ON $mode $temperature at $now"  >> /opt/scripts/logs.txt
-		curl -s -H "Authorization: Bearer $token" -H "Accept: application/json" -H "Content-Type: application/json; charset=UTF-8" -H "Host: api-global-prod.aircloudhome.com" -H "User-Agent: okhttp/4.2.2" --data-binary "{\"fanSpeed\":\"$fanSpeed\",\"fanSwing\":\"BOTH\",\"humidity\":\"50\",\"id\":$roomId,\"iduTemperature\":$temperature.0,\"mode\":\"$mode\",\"power\":\"ON\"}" -X PUT --compressed "https://api-global-prod.aircloudhome.com/rac/basic-idu-control/general-control-command/$roomId?familyId=$familyId"
+		curl -s -H "Authorization: Bearer $token" -H "Accept: application/json" -H "Content-Type: application/json; charset=UTF-8" -H "Host: api-global-prod.aircloudhome.com" -H "User-Agent: okhttp/4.2.2" --data-binary "{\"fanSpeed\":\"$fanSpeed\",\"fanSwing\":\"$fanSwing\",\"humidity\":\"0\",\"id\":$roomId,\"iduTemperature\":$temperature,\"mode\":\"$mode\",\"power\":\"ON\"}" -X PUT --compressed "https://api-global-prod.aircloudhome.com/rac/basic-idu-control/general-control-command/$roomId?familyId=$familyId"
 		sleep 20
 		websocatresult=$(echo $connectandsub | $websocatbinary -b --base64 --ping-timeout=$pingtimeout -q -n $wssairCloud | grep -a HITACHI | tr -d "\n")
 	done
@@ -71,7 +130,7 @@ case "$1" in
 	until [ $(echo $connectandsub | $websocatbinary -b --base64 --ping-timeout=$pingtimeout -q -n $wssairCloud | grep -a HITACHI | tr -d "\n" | jq -r ".data[] | select(.id==$roomId) | .power") = "OFF" ]
 	do
 		#echo "OFF $mode $temperature at $now"  >> /opt/scripts/logs.txt
-		curl -s -H "Authorization: Bearer $token" -H "Accept: application/json" -H "Content-Type: application/json; charset=UTF-8" -H "Host: api-global-prod.aircloudhome.com" -H "User-Agent: okhttp/4.2.2" --data-binary "{\"fanSpeed\":\"$fanSpeed\",\"fanSwing\":\"BOTH\",\"humidity\":\"50\",\"id\":$roomId,\"iduTemperature\":$temperature.0,\"mode\":\"$mode\",\"power\":\"OFF\"}" -X PUT --compressed "https://api-global-prod.aircloudhome.com/rac/basic-idu-control/general-control-command/$roomId?familyId=$familyId"
+		curl -s -H "Authorization: Bearer $token" -H "Accept: application/json" -H "Content-Type: application/json; charset=UTF-8" -H "Host: api-global-prod.aircloudhome.com" -H "User-Agent: okhttp/4.2.2" --data-binary "{\"fanSpeed\":\"$fanSpeed\",\"fanSwing\":\"$fanSwing\",\"humidity\":\"0\",\"id\":$roomId,\"iduTemperature\":$temperature,\"mode\":\"$mode\",\"power\":\"OFF\"}" -X PUT --compressed "https://api-global-prod.aircloudhome.com/rac/basic-idu-control/general-control-command/$roomId?familyId=$familyId"
 		sleep 20
 	done
   fi
